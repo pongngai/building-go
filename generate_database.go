38f058680c6629d8ce0167fc0728cad47c04c7a0
acn_varunyou.tarnwuttipong <acn_varunyou.tarnwuttipong@krungthai.com>
2023-08-10 17:20:00 +0700

    initial db template

---
+++ generate_database.go
@@ -0,0 +1,35 @@
+// generate.go
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	_const "github.com/pongngai/building-go/const"
+	"log"
+	"os"
+	_ "os/exec"
+	"text/template"
+)
+
+func main() {
+
+	templateStr := _const.Header + _const.DatabaseTemplate
+
+	// Create a new template and parse the template string
+	tmpl := template.Must(template.New("generated").Parse(templateStr))
+
+	// Create the output file
+	outputFile, err := os.Create(_const.DatabaseGeneratedPath)
+	if err != nil {
+		log.Fatal("Failed to create output file:", err)
+	}
+	defer outputFile.Close()
+
+	// Execute the template and write the generated code to the output file
+	err = tmpl.Execute(outputFile, nil)
+	if err != nil {
+		log.Fatal("Template execution failed:", err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pongngai/building-go
+
+go 1.19
